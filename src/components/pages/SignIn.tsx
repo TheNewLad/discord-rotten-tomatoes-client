73aa228f5ac12b4f76735e51d24a2e5a7bbcd7da
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-06-01 00:07:57 -0500

    feat: remove old config

--- src/components/pages/SignIn.tsx
+++
@@ -1,7 +0,0 @@
-import { SignIn as ClerkSignIn } from "@clerk/clerk-react";
-
-export const SignIn = () => (
-  <div className="grid h-screen place-items-center">
-    <ClerkSignIn />
-  </div>
-);
--- src/components/ui/onboarding/onboardingSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const onboardingSchema = z.object({
-  plot: z.number().min(0).max(10),
-  acting: z.number().min(0).max(10),
-  visuals: z.number().min(0).max(10),
-  audio: z.number().min(0).max(10),
-  pacing: z.number().min(0).max(10),
-});
