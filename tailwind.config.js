b1ab950837d6ec5ea76fff79c26ecdf97bbdbae3
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-06-12 23:53:08 -0500

    fix: remove darkMode property to allow it to work properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ import forms from "@tailwindcss/forms";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   prefix: "",
   theme: {
